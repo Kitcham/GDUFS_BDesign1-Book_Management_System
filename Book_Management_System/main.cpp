8e772525b4dbb68067f916697cbcc3c69f219b47
Kitcham <kitcham@outlook.com>
2019-04-05 00:43:28 +0800

    Revert "Revert "feat(out_file): add files write out""
    This reverts commit 4e8f10fe9a039370b4f84eaa7b0538d7b8c44a7f.

--- Book_Management_System/main.cpp
+++ Book_Management_System/main.cpp
@@ -2,12 +2,16 @@
 #include"FilesOpera.h"
 #include"LinkList.h"
 
-int main() {
+int main()
+{
 	int a, choose;
 	bool b = true;
 	int count = 1;
+	int total;
+	std::ofstream writefile;
 	std::string ISBNTEMP;
 	std::string bknametmp;
+	std::string buku;
 	Book e;
 	DuLinkList L = nullptr, p = nullptr;
 	std::cout << "请输入你所需功能的数字编号\n";
@@ -25,10 +29,12 @@ int main() {
 	std::cout << "0.  退出系统\n\n";
 
 	choose = -1;
-	while (choose != 0) {
+	while (choose != 0)
+	{
 		std::cout << "你处于主菜单中，请选择:";
 		std::cin >> choose;
-		switch (choose) {
+		switch (choose)
+		{
 		case 1: //建立一个双向链表
 			if (InitDuList_L(L))
 				std::cout << "成功建立图书信息存储列表，存储结构为双向链表!\n\n";
@@ -39,7 +45,7 @@ int main() {
 			break;
 		case 3: //双向链表的插入
 			std::cout << "请输入新增图书的信息:\n";
-			std::cout << "请输入插入的位置:"; std::cin >> a; 
+			std::cout << "请输入插入的位置:"; std::cin >> a;
 			while (std::cin.fail())           //判断cin是否failbit，即输入了非整型数据
 			{
 				std::cin.clear();             //是则置空并清除缓冲区，以免死循环
@@ -63,7 +69,7 @@ int main() {
 			std::cout << "请输入你要修改的图书的ISBN号:";
 			std::cin >> ISBNTEMP;
 			p = ISBNcompare(L, ISBNTEMP);
-			if(p)
+			if (p)
 			{
 				std::cout << "检索成功，当前书名为：《" << p->data.name << "》\t" << "当前价格为：" << p->data.price << std::endl;
 				std::cout << "请输入新价格：";
@@ -95,12 +101,11 @@ int main() {
 				std::cout << "不好意思，目前任何图书信息\n";
 			break;
 		case 7: //检索图书
-			std::cout << "请输入一个关键字以检索图书（不含空格）:"; 
+			std::cout << "请输入一个关键字以检索图书（不含空格）:";
 			std::cin >> bknametmp;
 			FindBk(L, bknametmp);
 			break;
 		case 8://出借图书
-		{
 			std::cout << "请输入您想要借的书的序号:" << std::endl;
 			int n; int store_num;
 			std::cin >> n;
@@ -121,47 +126,53 @@ int main() {
 			int shu;
 			std::cin >> shu;
 			Change_store(L, n, shu);
-			break; }
+			break;
 		case 9:// 归还图书
-		{
 			std::cout << "请输入您要归还的书的ISBN号" << std::endl;
-			std::string buku;
 			std::cin >> buku;
-			int total = 0;
+			total = 0;
 			std::cout << "请输入您要归还的书的数量" << std::endl;
 			std::cin >> total;
 			Return_book(L, buku, total);
-
-		}
-		case 10:
-		{//双向链表的输出
+		case 10://双向链表的输出
 			std::cout << "当前图书系统信息输出:\n";
-			std::cout << "序号"<<"\t"<<std::left << std::setw(15) << "ISBN号" << std::left << std::setw(50) << "书名" << "\t" << std::left << std::setw(5) << "价格" << "\t" << std::left << std::setw(15) << "数量" << std::endl;
+			std::cout << "序号" << "\t" << std::left << std::setw(15) << "ISBN号" << std::left << std::setw(50) << "书名" << "\t" << std::left << std::setw(5) << "价格" << "\t" << std::left << std::setw(15) << "数量" << std::endl;
 			//L = new DuLinkList;
 			p = L->next;
-			int count = 0;
-			while (p) {
+			count = 0;
+			while (p)
+			{
 				//count = 1;
 				std::cout << ++count << ")" << "\t" << std::left << std::setw(15) << p->data.id << "\t" << std::left << std::setw(50) << p->data.name << "\t" << std::left << std::setw(5) << p->data.price << "\t" << std::left << std::setw(5) << p->data.number << std::endl;
-				std::cout << "----------------------------------------------------------------------------------------------" <<std::endl;
+				std::cout << "----------------------------------------------------------------------------------------------" << std::endl;
 				p = p->next;
 			}
 			p = Total(L);
-			std::cout << std::left << std::setw(15) << "总计" << std::left << std::setw(50) << "   " << "\t\t" << std::left << std::setw(5)<< p->data.price << "\t" << std::left << std::setw(15) << p->data.number << std::endl;
+			std::cout << std::left << std::setw(15) << "总计" << std::left << std::setw(50) << "   " << "\t\t" << std::left << std::setw(5) << p->data.price << "\t" << std::left << std::setw(15) << p->data.number << std::endl;
 			std::cout << std::endl;
 			break;
-		}
-		case 11:
-			int result;
-			result = Store_the_content(L);
-			if (!result)
+		case 11://双向链表的写文件
+			writefile.open("new book.txt");
+			std::cout << "图书系统正在进行信息输出:\n";
+			writefile << "当前图书系统信息输出:\n";
+			writefile << "序号" << "\t" << std::left << std::setw(15) << "ISBN号" << std::left << std::setw(50) << "书名" << "\t" << std::left << std::setw(5) << "价格" << "\t" << std::left << std::setw(15) << "数量" << std::endl;
+			//L = new DuLinkList;
+			p = L->next;
+			count = 0;
+			while (p)
 			{
-				std::cout << "存盘失败\n\n";
+				//count = 1;
+				writefile << ++count << ")" << "\t" << std::left << std::setw(15) << p->data.id << "\t" << std::left << std::setw(50) << p->data.name << "\t" << std::left << std::setw(5) << p->data.price << "\t" << std::left << std::setw(5) << p->data.number << std::endl;
+				writefile << "----------------------------------------------------------------------------------------------" << std::endl;
+				p = p->next;
 			}
-			else
-			{
-				std::cout << "存盘成功\n\n";
-			}
+			p = Total(L);
+			writefile << std::left << std::setw(15) << "总计" << std::left << std::setw(50) << "   " << "\t\t" << std::left << std::setw(5) << p->data.price << "\t" << std::left << std::setw(15) << p->data.number << std::endl;
+			std::cout << std::endl;
+			writefile << std::endl;
+			writefile.close();
+			std::cout << "图书系统输出完成\n";
+			break;
 		}
 	}
 	return 0;
